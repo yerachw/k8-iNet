fe17fb9f21469bc1d619c87034b10ecdfcc82925
Ronnie Wulfsohn <yerachw@hotmail.com>
2019-10-22 11:44:43 +0300

    Trying sql

--- kubernetes/label_nodes.sh
+++ kubernetes/label_nodes.sh
@@ -10,3 +10,8 @@ kubectl label nodes kibana target=kibana
 kubectl label nodes standby1 target=standby
 kubectl label nodes standby2 target=standby
 kubectl label nodes master target=master
+
+kubectl label nodes storage storageos.com/deployment=mixed
+kubectl label nodes mysql storageos.com/deployment=computeonly
+kubectl label nodes elastic storageos.com/deployment=computeonly
+kubectl label nodes redis storageos.com/deployment=computeonly
